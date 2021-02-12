7441c911ca412a483e45cbd1e401f9175b969027
Bernard <bh20@hw.ac.uk>
2021-02-12 16:49:32 +0000

    Started to add scripts

---
+++ Assets/Script/CarController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    public Rigidbody theRB;
+
+    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = theRB.transform.position;
+    }
+
+    private void fixedUpdate()
+    {
+        theRB.AddForce(transform.forward * forwardAccel);
+    }
+}
